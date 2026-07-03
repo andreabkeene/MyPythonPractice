7f4f1c3d5433cb64430c4347f67d63d9547c659e
andrea_keene <andrea.b.keene@gmail.com>
2026-07-02 19:10:01 -0700

    we have asteroids now

--- pygamelesson/main.py
+++ pygamelesson/main.py
@@ -2,6 +2,8 @@ import pygame
 from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -18,8 +20,13 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroid_field = AsteroidField()
 
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
