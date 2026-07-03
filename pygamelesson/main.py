fee695268117f4756393aff410c6be88c1b0c9d8
andrea_keene <andrea.b.keene@gmail.com>
2026-07-02 17:01:10 -0700

    screen items in groups

--- pygamelesson/main.py
+++ pygamelesson/main.py
@@ -1,5 +1,6 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
 
 
@@ -15,6 +16,15 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x,y)
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -22,9 +32,13 @@ def main():
                 return
         
         screen.fill("black")
+        updatable.update(dt)
+        for drawable_item in drawable:
+            drawable_item.draw(screen)
         pygame.display.flip()
-
+        
         dt = clock.tick(60)/1000
+
 
 
 if __name__ == "__main__":
